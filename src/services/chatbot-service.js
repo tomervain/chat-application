8f41be0e094065f575c2ec5711fd7347db018aa4
tomervain <tomer.vainstein@gmail.com>
2022-02-28 17:51:51 +0200

    add elasticsearch service

--- src/services/chatbot-service.js
+++ src/services/chatbot-service.js
@@ -1,10 +1,15 @@
 const io = require("socket.io-client");
 
+const { ElasticsearchService } = require('./elasticsearch-service');
+
 class ChatbotService {
     constructor(botName, port) {
         this.name = botName,
         this.port = port
         this.socket = io(`ws://localhost:${this.port}`);
+        this.elasticService = new ElasticsearchService();
+        this.questionFlag = false;
+        this.question = "";
     }
 
     initialize() {
@@ -13,12 +18,41 @@ class ChatbotService {
     }
 
     socketInit() {
-        this.socket.on('chatMessage', message => {
-            if (message.user != this.name && message.text.includes('bones')) {
-                this.socket.emit('chatMessage', {
-                    user: this.name,
-                    text: "mmmmm... bonessss"
-                });
+        this.socket.on('chatMessage', async (message) => {
+            // don't respond to own message
+            if (message.user === this.name)
+                return;
+
+            // if question flag is on
+            if (this.questionFlag) {
+                // and another question came, override
+                if (message.text.includes('?')) {
+                    this.question = message.text;
+                } else {                    
+                    // else its considered a new answer
+                    await this.elasticService.saveQuestionAndAnswer(this.question, message.text);
+                    this.question = "";
+                    this.questionFlag = false;
+                }
+
+                return;
+            } else if (message.text.includes('?')) {
+                // look for answer, if exist answer it
+                const answer = await this.elasticService.lookForAnswer(message.text);
+                if (answer != null) {
+                    this.socket.emit('chatMessage', {
+                        user: this.name,
+                        text: answer
+                    });
+
+                    this.question = "";
+                    this.questionFlag = false;
+                    return;
+                }
+
+                this.question = message.text;
+                this.questionFlag = true;
+                return;
             }
         });
     }
---
+++ src/services/elasticsearch-service.js
@@ -0,0 +1,40 @@
+const { Client } = require('@elastic/elasticsearch');
+
+const NODE = 'http://localhost:9200';
+const USER = 'elastic'
+const INDEX = 'chatapp'
+
+class ElasticsearchService {
+    constructor() {
+        this.client = new Client({ node: NODE })
+    }
+
+    async saveQuestionAndAnswer(question, answer) {
+        await this.client.index({
+            index: INDEX,
+            document: {
+                question: question,
+                answer: answer
+            }
+        });
+
+        await this.client.indices.refresh({ index: INDEX });
+    }
+
+    async lookForAnswer(question) {
+        const results = await this.client.search({
+            index: INDEX,
+            query: { match: { question: question } }
+        });
+
+        if (results.hits.total.value === 0)
+            return null;
+        
+        if (results.hits.hits[0]._score < 0.5)
+            return null; // probably not related to question 
+        
+        return results.hits.hits[0]._source.answer;
+    }
+}
+
+exports.ElasticsearchService = ElasticsearchService;
